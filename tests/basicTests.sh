d82978c862c299270c44399a1958178f0e451896
Dale Henrichs <dale.henrichs@gemtalksystems.com>
2015-03-25 20:31:06 -0700

    main thing lock tODE for pharo client

--- tests/basicTests.sh
+++ tests/basicTests.sh
@@ -5,5 +5,15 @@
 
 set -e  # exit on error
 
-# create a sample stone
+# Verify that the expressions in the readme run without error
+
 $SAMPLE_HOME/bin/installSampleServer sample $GS_VERSION
+$SAMPLE_HOME/bin/sampleGemServer sample --restart=sample
+
+$GS_HOME/bin/stoneNewExtent -s $GS_HOME/gemstone/stones/sample/snapshots/extent0.sample.dbf sample
+
+$SAMPLE_HOME/bin/createSampleStone -s $GS_HOME/gemstone/stones/sample/snapshots/extent0.sample.dbf sample2 3.2.4
+
+$SAMPLE_HOME/bin/createSampleStone -s $GS_HOME/gemstone/stones/sample/snapshots/extent0.tode.dbf sample2 3.2.4
+
+$SAMPLE_HOME/bin/createSampleStone -s $GEMSTONE/bin/extent0.seaside.dbf sample 3.2.4
